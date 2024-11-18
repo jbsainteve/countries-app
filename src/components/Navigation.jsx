f8a9ccc1f3dff9aea7915a2b70ff82af26449d0f
jbsainteve <jbsainteve@yahoo.fr>
2024-11-18 19:52:16 +0100

    add img

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,6 +4,7 @@ import { NavLink } from "react-router-dom";
 const Navigation = () => {
   return (
     <div className="navigation">
+      <img src="./img/poster-blockchain.jpg" alt="" />
       <ul>
         <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
           <li>Home</li>
